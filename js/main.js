d181ce4268fad281f3fdb96f774e15760df08c63
Juan <j.tejeda46@yahoo.com>
2023-12-16 00:48:05 -0600

    fixed hovering issue

--- js/main.js
+++ js/main.js
@@ -3,18 +3,25 @@ console.log('Welcome to the general store!')
 
 // Grab Item ID from Item name input
 const geItemAPI = async function(itemName){
-    let information = await fetch('https://oldschool.runescape.wiki/?title=Module:GEIDs/data.json&action=raw&ctype=application%2Fjson')
-    let infoData = await information.json()
-    return await infoData[itemName]
+    try{
+        let information = await fetch('https://oldschool.runescape.wiki/?title=Module:GEIDs/data.json&action=raw&ctype=application%2Fjson');
+        let infoData = await information.json();
+        return await infoData[itemName];
+    } catch(error){
+        console.error('Error loading JSON:', error);
+    }   
 }
-console.log(geItemAPI('Acorn'))
 
 
 // Get Item Prices
 const gePriceAPI = async function(itemId){
-    let prices = await fetch(`https://prices.runescape.wiki/api/v1/osrs/latest?id=${itemId}`)
-    let priceData = await prices.json()
-    return await priceData
+    try{
+        let prices = await fetch(`https://prices.runescape.wiki/api/v1/osrs/latest?id=${itemId}`)
+        let priceData = await prices.json()
+        return await priceData
+    } catch(error){
+        console.error('Error loading JSON:', error)
+    }
 }
 
 const searchItem = document.getElementById('geForm')
@@ -43,52 +50,53 @@ const updateCard = function(item, gePrice, itemN){
 
 }
 
+const itemInput = document.getElementById('itemInput');
+const suggestionsList = document.getElementById('suggestions');
 
-document.addEventListener('DOMContentLoaded', function () {
-    const itemInput = document.getElementById('itemInput');
-    const suggestionsList = document.getElementById('suggestions');
+// Asynchronous function to load JSON data
+async function loadItems() {
+    try {
+        const response = await fetch('https://oldschool.runescape.wiki/?title=Module:GEIDs/data.json&action=raw&ctype=application%2Fjson');
+        const itemsObject = await response.json();
 
-    // Asynchronous function to load JSON data
-    async function loadItems() {
-        try {
-            const response = await fetch('https://oldschool.runescape.wiki/?title=Module:GEIDs/data.json&action=raw&ctype=application%2Fjson');
-            const itemsObject = await response.json();
-
-            itemInput.addEventListener('input', function () {
-                const userInput = itemInput.value.toLowerCase().trim();
-                const itemsArray = Object.entries(itemsObject);
-                const filteredItems = itemsArray.slice(2).filter(([key, value]) =>
-                    key.toString().toLowerCase().includes(userInput)
-                );
-                displaySuggestions(filteredItems.slice(0,10));
-            });
-        } catch (error) {
-            console.error('Error loading JSON:', error);
-        }
-    }
-
-    loadItems();
-
-    function displaySuggestions(suggestedItems) {
-        suggestionsList.innerHTML = '';
-
-        suggestedItems.forEach(([key]) => {
-            const listItem = document.createElement('li');
-            const imageEl = document.createElement('img');
-            listItem.textContent = ` ${key}`;
-            imageEl.src = `https://oldschool.runescape.wiki/images/${key.replace(/\s+/g, '_')}_detail.png`;
-            imageEl.style.width = '35px';
-            imageEl.style.height = '35px';
-            listItem.insertBefore(imageEl, listItem.firstChild);
-            listItem.classList.add('list-group-item');
-            
-            // Add click event listener to fill in the input on click
-            listItem.addEventListener('click', function () {
-                itemInput.value = key;
-                suggestionsList.innerHTML = ''; // Clear suggestions after selecting one
-            });
-
-            suggestionsList.appendChild(listItem);
+        itemInput.addEventListener('input', function () {
+            const userInput = itemInput.value.toLowerCase().trim();
+            const itemsArray = Object.entries(itemsObject);
+            const filteredItems = itemsArray.slice(2).filter(([key, value]) =>
+                key.toString().toLowerCase().includes(userInput)
+            );
+            console.log(filteredItems)
+            displaySuggestions(filteredItems.slice(0,6));
         });
+    } catch (error) {
+        console.error('Error loading JSON:', error);
     }
-});
+}
+
+loadItems();
+
+function displaySuggestions(suggestedItems) {
+    suggestionsList.innerHTML = '';
+
+    suggestedItems.forEach(([key]) => {
+        const listItem = document.createElement('a');
+        const imageEl = document.createElement('img');
+        listItem.textContent = ` ${key}`;
+        imageEl.src = `https://oldschool.runescape.wiki/images/${key.replace(/\s+/g, '_')}_detail.png`;
+        imageEl.style.width = '35px';
+        imageEl.style.height = '35px';
+        listItem.insertBefore(imageEl, listItem.firstChild);
+        listItem.classList.add('list-group-item', 'list-group-item-action')
+        listItem.href = '#'
+        
+        // Add click event listener to fill in the input on click
+        listItem.addEventListener('click', function () {
+            itemInput.value = key;
+            suggestionsList.innerHTML = ''; // Clear suggestions after selecting one
+        });
+
+        suggestionsList.appendChild(listItem);
+    });
+}
+
+
